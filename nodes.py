e75e6d69339675bc549e4fef360fb0b4529ce4be
kijai <40791699+kijai@users.noreply.github.com>
2025-04-25 16:19:52 +0300

    Update nodes.py

--- nodes.py
+++ nodes.py
@@ -2761,8 +2761,8 @@ class WanVideoSampler:
                         if fun_ref_image is not None:
                             fun_ref_input = fun_ref_image.to(z)
                         else:
-                            #fun_ref_input = torch.zeros_like(z, dtype=z.dtype)[:, 0].unsqueeze(1)
-                            fun_ref_input = None
+                            fun_ref_input = torch.zeros_like(z, dtype=z.dtype)[:, 0].unsqueeze(1)
+                            #fun_ref_input = None
 
                     if control_lora:
                         if not control_start_percent <= current_step_percentage <= control_end_percent:
